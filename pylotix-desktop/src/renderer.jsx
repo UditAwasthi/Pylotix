891668216fc262e5cbbd20004d5493749c8a72f4
Udit Awasthi <nikhilawasthi19jan@gmail.com>
2026-02-15 20:05:39 +0530

    initial commit

---
+++ pylotix-desktop/src/renderer.jsx
@@ -0,0 +1,11 @@
+
+import {createRoot} from 'react-dom/client';
+
+
+const App = () =>{
+  return <h1>Hello, Pylotix!</h1>
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App/>);
